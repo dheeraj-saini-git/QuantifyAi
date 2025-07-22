88a3e61a18274f65bf3d08a7df8f1f16231b7b97
dheeraj-saini-git <149778591+dheeraj-saini-git@users.noreply.github.com>
2025-07-22 21:24:31 +0530

    Add Hero, AiTools, Testimonial components and update Home & Navbar layout
    - Added new components: Hero, AiTools, Testimonial
    - Updated Home.jsx to include new sections
    - Refined Navbar UI
    - Added background image to public folder
    - Installed/updated packages
    - Created initial .env file

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
-
+import Hero from '../Components/Hero'
+import AiTools from '../Components/AiTools'
+import Testimonial from '../Components/Testimonial'
 
 const Home = () => {
   return (
     <>
       <Navbar/>
+      <Hero />
+      <AiTools />
+      <Testimonial />
     </>
   )
 }
