254392c92989ab861d3c63520434d50d45a93b86
dheeraj-saini-git <149778591+dheeraj-saini-git@users.noreply.github.com>
2025-08-04 21:59:22 +0530

    integrated and added database, authetication with clerk and created folders like middlewares controllers

--- server/server.js
+++ server/server.js
@@ -1,14 +1,19 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config.js'
+import { clerkMiddleware, requireAuth } from '@clerk/express'
 
 const app = express()
 const PORT = process.env.PORT|| 3000
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json()),
+app.use(clerkMiddleware())
 
 app.get('/', (req,res)=> res.send('Server is Live!'))
+app.use(requireAuth())
+app.get('/auth', (req,res)=> res.send('Server is authenticated!'))
+
 
 app.listen(PORT, ()=>{
     console.log('Server is running on port', PORT)
