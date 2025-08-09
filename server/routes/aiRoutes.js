35d9967b5d0a1af2952371ac53380529af51fed0
dheeraj-saini-git <149778591+dheeraj-saini-git@users.noreply.github.com>
2025-08-09 21:05:37 +0530

    created routes for newly endpoints

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import { auth } from '../middlerwares/auth.js'
-import { generateArticle, generateBlogTitle, generateImage } from '../controllers/aiController.js'
-
+import { generateArticle, generateBlogTitle, generateImage, removeImageBackground, removeImageObject, resumeReview } from '../controllers/aiController.js'
+import upload from '../configs/multer.js'
 
 const router = express.Router()
 
 router.post('/generate-article', auth, generateArticle)
 router.post('/generate-blog-title', auth, generateBlogTitle)
 router.post('/generate-image', auth, generateImage)
+router.post('/remove-image-background', auth, upload.single('image'), removeImageBackground)
+router.post('/remove-image-object', auth, upload.single('image'), removeImageObject)
+router.post('/resume-review', auth, upload.single('resume'), resumeReview)
 
 
 export default router
